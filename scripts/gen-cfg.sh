d0b3e15a92e231abb4a4f4d96426b63e2881c3a1
David Flynn <dflynn@blackberry.com>
2018-05-23 12:09:10 +0200

    cfg: add N17523 common test condition configuration files
    This commit updates the master configuration files to support:
    
     - new test conditions for cat3 tests
     - existing conditions for cat1 tests (for use by tmc13)
     - updates enc/decflags to use recommended forms
     - integrates pcerror flags (per condition / sequence)
     - adds sequence databases describing source files, and source
       parameters that may be used by other scripts.
    
    The ctc-category-map.csv file has been removed -- a new version can be
    provided if necessary.

--- scripts/gen-cfg.sh
+++ scripts/gen-cfg.sh
@@ -4,11 +4,24 @@
 # directory.
 
 set -e
+shopt -s nullglob
 
 script_dir=$(dirname $0)
+src_cfg_dir="${1}${1:+/}"
+shift || true;
 
-for f in ctc-*.yaml
+for f in ${src_cfg_dir}cat3-tmc13-ctc-*.yaml
 do
-	$script_dir/gen-cfg.pl --no-skip-sequences-without-src $f
+	echo $f ...
+	$script_dir/gen-cfg.pl --no-skip-sequences-without-src "$@" $f
+	rm config-merged.yaml
+done
+
+
+# NB, cat1 configs need the sequence parameters
+for f in ${src_cfg_dir}cat1-tmc13-ctc-*.yaml
+do
+	echo $f ...
+	$script_dir/gen-cfg.pl --no-skip-sequences-without-src "$@" $f ${src_cfg_dir}sequences-cat1.yaml
 	rm config-merged.yaml
 done
